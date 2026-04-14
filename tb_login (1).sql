fc517f0184e8f60a7cc39c4639bd55a846bf84b4
fantri <ifan.ifzn17@gmail.com>
2026-04-14 08:34:27 +0700

    change_role

--- tb_login (1).sql
+++ tb_login (1).sql
@@ -3,14 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2026 at 03:12 AM
+-- Generation Time: Apr 14, 2026 at 03:33 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
- 
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_sesion`
+-- Database: `db_login`
 --
 
 -- --------------------------------------------------------
@@ -31,7 +31,7 @@ CREATE TABLE `tb_login` (
   `id_user` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` enum('admin','manajer') NOT NULL DEFAULT 'manajer'
+  `role` enum('admin','manager') NOT NULL DEFAULT 'manager'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -43,10 +43,8 @@ INSERT INTO `tb_login` (`id_user`, `username`, `password`, `role`) VALUES
 (2, 'budi', '9c5fa085ce256c7c598f6710584ab25d', ''),
 (3, 'sari', 'e9ee75b57bb1303190c8869621cad05b', ''),
 (4, 'aslkjd', 'e10adc3949ba59abbe56e057f20f883e', 'admin'),
-(5, 'ifan', '8485030acbb7ff246b7bff2884f02c1d', 'manajer'),
-(6, 'akbar', 'f039e5f60e85d10bf7b742e65ad931ca', ''),
-(7, 'abdil', '49573db154650436a32e14e10f7a7ca7', 'manajer'),
-(8, 'iqbal', 'e10adc3949ba59abbe56e057f20f883e', 'admin');
+(10, 'ifan', '8485030acbb7ff246b7bff2884f02c1d', 'admin'),
+(11, 'akbar', 'f039e5f60e85d10bf7b742e65ad931ca', 'manager');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +65,7 @@ ALTER TABLE `tb_login`
 -- AUTO_INCREMENT for table `tb_login`
 --
 ALTER TABLE `tb_login`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
